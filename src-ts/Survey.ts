143565744bd593d2ed99244a8fced52c2fc72752
Kartikeya Kandula <kandula.kartikeya@gmail.com>
2021-05-25 10:27:20 -0400

    updated survey functionality

--- src-ts/Survey.ts
+++ src-ts/Survey.ts
@@ -3,9 +3,11 @@ import * as webScience from "@mozilla/web-science";
 const millisecondsPerSecond = 1000;
 const secondsPerDay = 86400;
 const surveyRemindPeriodDays = 3;
-const secondSurveyStartDay = 30;
+const daysUntilSecondSurvey = 30;
 
-const surveyData = {
+
+// TDOD: update these
+const surveyConfigData = {
   initial: {
     surveyName: "initial",
     popupNoPromptMessage: "There are currently no available surveys for the study.",
@@ -18,8 +20,8 @@ const surveyData = {
     surveyCompletionUrl: "https://citpsearch.cs.princeton.edu/searchengine/initial/thankyou",
     surveyUrl: "https://kartikeyakandula.com/",
   },
-  second: {
-    surveyName: "second",
+  final: {
+    surveyName: "final",
     popupNoPromptMessage: "No survey prompt message 2",
     popupPromptMessage: "popupPromptMessage 2",
     popupIcon: "icons/PrincetonShieldLarge.png",
@@ -30,35 +32,24 @@ const surveyData = {
     surveyCompletionUrl: "https://citpsearch.cs.princeton.edu/searchengine/final/thankyou",
     surveyUrl: "https://www.google.com/",
   },
-  secondPlus: {
-    surveyName: "secondPlus",
-    popupNoPromptMessage: "No survey prompt message 3",
-    popupPromptMessage: "popupPromptMessage 3",
-    popupIcon: "icons/PrincetonShieldLarge.png",
-    reminderIcon: "icons/PrincetonShieldLarge.png",
-    reminderInterval: surveyRemindPeriodDays * secondsPerDay,
-    reminderMessage: "reminderMessage 3",
-    reminderTitle: "reminderTitle 3",
-    surveyCompletionUrl: "https://citpsearch.cs.princeton.edu/searchengine/final/thankyou",
-    surveyUrl: "https://duckduckgo.com/",
-  },
 }
 
 let storage;
 
-async function startSecondSurvey() {
-  // We get the status of the initial survey before ending the survey and 
-  // starting one of the initial surveys.
-  const initialSurveyCompleted = await webScience.userSurvey.getSurveyStatus();
+async function startFinalSurvey() {
   await webScience.userSurvey.endSurvey();
-  if (initialSurveyCompleted === "completed") {
-    // If the initial survey was completed, we set the current survey to be
-    // the default second survey.
-    webScience.userSurvey.setSurvey(surveyData.second);
+  webScience.userSurvey.setSurvey(surveyConfigData.final);
+}
+
+// Max timeout for setTimeout is 0x7FFFFFFF milliseconds (slightly less than 24.9 days)
+// so this is needed for a longer timeout
+function setFinalSurveyTimeout(delay) {
+  if (delay > 0x7FFFFFFF) {
+    setTimeout(function () { setFinalSurveyTimeout(delay - 0x7FFFFFFF) }, 0x7FFFFFFF);
   } else {
-    // If the initial survey was not completed, we set the current survey to be
-    // the second survey with demographic questions from the initial survey
-    webScience.userSurvey.setSurvey(surveyData.secondPlus);
+    setTimeout(() => {
+      startFinalSurvey();
+    }, delay);
   }
 }
 
@@ -74,28 +65,24 @@ export async function runSurvey(storageIn): Promise<void> {
     storage.set("InitialSurveyStartTime", initialSurveyStartTime);
   }
 
-  let secondSurveyStartTime = initialSurveyStartTime + (millisecondsPerSecond * secondsPerDay * secondSurveyStartDay);
+  let finalSurveyStartTime = initialSurveyStartTime + (millisecondsPerSecond * secondsPerDay * daysUntilSecondSurvey);
 
   let currentSurvey = await webScience.userSurvey.getSurveyName();
-  console.log("SURVEY 1")
 
   if (!currentSurvey ||
-    (currentSurvey === surveyData.initial.surveyName && Date.now() <= secondSurveyStartTime)) {
+    (currentSurvey === surveyConfigData.initial.surveyName && Date.now() <= finalSurveyStartTime)) {
     // If there is no current survey or the current survey is the initial survey
-    // and the current time is before the time to start the second survey, we set
-    // the current survey to be the initial survey and set a timeout to start the second survey.
-    webScience.userSurvey.setSurvey(surveyData.initial);
-    console.log(secondSurveyStartTime - Date.now())
-    setTimeout(() => {
-      startSecondSurvey();
-    }, 10000);
-  } else if (currentSurvey === surveyData.initial.surveyName) {
-    // If the current survey is the initial survey, we start the second survey.
-    // We only reach here if the current time is after the second survey start time.
-    startSecondSurvey();
+    // and the current time is before the time to start the final survey, we set
+    // the current survey to be the initial survey and set a timeout to start the final survey.
+    webScience.userSurvey.setSurvey(surveyConfigData.initial);
+    setFinalSurveyTimeout(finalSurveyStartTime - Date.now());
+  } else if (currentSurvey === surveyConfigData.initial.surveyName) {
+    // If the current survey is the initial survey, we start the final survey.
+    // We only reach here if the current time is after the final survey start time.
+    startFinalSurvey();
   } else {
     // Set the survey to the current survey.
-    // We only reach here if the current survey is one of the second surveys.
-    webScience.userSurvey.setSurvey(surveyData[currentSurvey]);
+    // We only reach here if the current survey is the final survey.
+    webScience.userSurvey.setSurvey(surveyConfigData[currentSurvey]);
   }
 }
