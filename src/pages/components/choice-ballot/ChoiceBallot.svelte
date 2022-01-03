a333c354b74aaa3f9b10cd9f4c36670be7063e3e
Kartikeya Kandula <kandula.kartikeya@gmail.com>
2022-01-03 10:41:44 -0500

    Finalized treatment designs

--- src/pages/components/choice-ballot/ChoiceBallot.svelte
+++ src/pages/components/choice-ballot/ChoiceBallot.svelte
@@ -5,13 +5,13 @@
         {
             engine: "Google",
             engineDisplayName: "Google",
-            description:
-                "Find what you need with the world's most used search engine.",
+            description: "Google is the world's most used search engine.",
         },
         {
             engine: "DuckDuckGo",
             engineDisplayName: "DuckDuckGo",
-            description: "We don’t store your personal information. Ever.",
+            description:
+                "DuckDuckGo doesn't store your personal information. Ever.",
         },
         {
             engine: "Bing",
@@ -21,9 +21,9 @@
         },
         {
             engine: "Yahoo",
-            engineDisplayName: "Yahoo",
+            engineDisplayName: "Yahoo!",
             description:
-                "Yahoo Search helps you find the information you need.",
+                "Yahoo! Search helps you find the information you need.",
         },
         {
             engine: "Yandex",
@@ -40,7 +40,7 @@
             engine: "Ecosia",
             engineDisplayName: "Ecosia",
             description:
-                "Your searches plant trees in some of the harshest places on Earth.",
+                "Ecosia uses the ad revenue from your searches to plant trees.",
         },
         {
             engine: "Ask",
@@ -51,73 +51,90 @@
     ];
 </script>
 
-<section class="hero is-medium wrapper choice-ballot-wrapper">
-    <div class="columns">
-        <div class="column is-12">
-            <div class="choice-ballot-description has-text-centered">
-                <h1><strong>Select a Search Engine for Firefox</strong></h1>
+<section class="hero is-medium choice-ballot-wrapper">
+    <div
+        class="choice-ballot-description has-text-centered columns is-centered"
+    >
+        <div class="column is-9">
+            <h1>
+                <strong>
+                    Please Select a Default Search Engine for Firefox
+                </strong>
+            </h1>
+            <div id="homepageChangeDescription" class="hiding">
                 <h2>
-                    These search engines are each used by millions of users
-                    worldwide.
-                </h2>
-                <div id="homepageChangeDescription" class="hiding">
-                    <h2>
-                        Your homepage will be changed to Firefox Home as well.
-                    </h2>
-                    <h2>
-                        You can always change your search engine and homepage
-                        later in Firefox settings.
-                        <a
-                            href="https://support.mozilla.org/en-US/kb/firefox-options-preferences-and-settings"
-                            target="_blank">Learn More.</a
-                        >
-                    </h2>
-                </div>
-                <div id="noHomepageChangeDescription">
-                    <h2>
-                        You can always change your search engine later in
-                        Firefox settings.
-                        <a
-                            href="https://support.mozilla.org/en-US/kb/change-your-default-search-settings-firefox"
-                            target="_blank">Learn More.</a
-                        >
-                    </h2>
-                </div>
-            </div>
-            <div class="search-engines-wrapper">
-                {#each engines as { engine, engineDisplayName, description }, i}
-                    {#if choiceBallotType === "Extended"}
-                        <Entry
-                            {engine}
-                            {engineDisplayName}
-                            {description}
-                            {choiceBallotType}
-                        />
-                    {:else if i < 4}
-                        <Entry
-                            {engine}
-                            {engineDisplayName}
-                            {description}
-                            {choiceBallotType}
-                        />
-                    {/if}
-                {/each}
-            </div>
-            {#if choiceBallotType === "Extended"}
-                <div class="search-engines-more-wrapper hiding" />
-                <div class="content has-text-centered see-more-container">
-                    <!-- svelte-ignore a11y-missing-attribute -->
-                    <a class="button is-link is-rounded see-more"
-                        >See More Search Engines</a
+                    The home page for Firefox will also be changed to Firefox
+                    Home. You can change the default search engine and home page
+                    at any time in Firefox settings.
+                    <a
+                        href="https://support.mozilla.org/en-US/kb/firefox-options-preferences-and-settings"
+                        target="_blank"
+                        style="white-space:nowrap">Learn More.</a
                     >
-                </div>
-            {/if}
+                </h2>
+            </div>
+            <div id="noHomepageChangeDescription">
+                <h2>
+                    You can change the default search engine at any time in
+                    Firefox settings.
+                    <a
+                        href="https://support.mozilla.org/en-US/kb/change-your-default-search-settings-firefox"
+                        target="_blank"
+                        style="white-space:nowrap">Learn More.</a
+                    >
+                </h2>
+            </div>
         </div>
     </div>
+    <div class="search-engines-wrapper">
+        {#each engines as { engine, engineDisplayName, description }, i}
+            {#if choiceBallotType === "Extended"}
+                <Entry
+                    {engine}
+                    {engineDisplayName}
+                    {description}
+                    {choiceBallotType}
+                />
+            {:else if i < 4}
+                <Entry
+                    {engine}
+                    {engineDisplayName}
+                    {description}
+                    {choiceBallotType}
+                />
+            {/if}
+        {/each}
+    </div>
+    {#if choiceBallotType === "Extended"}
+        <div class="search-engines-more-wrapper hiding" />
+        <div class="content has-text-centered see-more-container">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="button is-rounded see-more">See More Search Engines</a>
+        </div>
+    {/if}
+
+    <!-- <div class="columns is-mobile is-centered"> -->
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <!-- <a class="button continue" disabled>Continue</a> -->
+    <!-- </div> -->
 </section>
-<footer class="footer">
-    <div class="content has-text-centered">
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a class="button is-link continue" disabled>Continue</a>
+
+<footer id="footer" class="has-text-centered">
+    <div style="background-color: #f0f0f4;">
+        <button class="button continue" disabled>Continue</button>
+    </div>
+
+    <div class="columns footer-notice" style="margin-bottom: 0;">
+        <div class="column is-10 is-offset-1">
+            <div class="has-text-centered">
+                <h4>
+                    You are receiving this notice because of a Mozilla Rally
+                    study that you previously joined: Search Engine Usage and
+                    Result Quality, by Princeton University researchers. You can
+                    learn more about the study or remove yourself from the study
+                    at any time in Mozilla Rally settings.
+                </h4>
+            </div>
+        </div>
     </div>
 </footer>
