b57945eee8e3ff9b211cf6255948f89a333a7b16
Kartikeya Kandula <kandula.kartikeya@gmail.com>
2022-03-13 16:02:15 -0400

    Added comments to ContentScripts.ts

--- src/ContentScripts.ts
+++ src/ContentScripts.ts
@@ -25,6 +25,7 @@ export async function registerContentScripts(conditionType, treatmentStartTime) 
     });
   });
 
+  // Register all of the non-Google SERP scripts
   for (const serpScript of serpScripts) {
     serpScript.args["runAt"] = "document_start";
     await browser.contentScripts.register(serpScript.args);
@@ -35,6 +36,8 @@ export async function registerContentScripts(conditionType, treatmentStartTime) 
 
 async function registerGoogleScript(conditionType, treatmentStartTime): Promise<void> {
 
+  // If the participant is not in the self preferenced result removal or replacement group,
+  // we register the default Google SERP script.
   if (conditionType !== "SelfPreferencedRemoval" && conditionType !== "SelfPreferencedReplacement") {
     registerGoogleDefaultScript();
     return;
@@ -42,11 +45,13 @@ async function registerGoogleScript(conditionType, treatmentStartTime): Promise<
 
   const currentTime = webScience.timing.now();
 
-  // We stop modifying self preferenced results after 50 days
+  // We stop modifying self preferenced results 50 days after the treatment start time.
   // (50 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
   const treatmentEndTime = treatmentStartTime + (50 * 24 * 60 * 60 * 1000);
 
   if (currentTime < treatmentStartTime) {
+    // If the current time is less than the treatment start time, we register the default Google SERP script
+    // and then set a timer to register the modification Google SERP script at the treatment start time.
     registerGoogleDefaultScript();
 
     setExtendedTimeout(() => {
@@ -54,28 +59,36 @@ async function registerGoogleScript(conditionType, treatmentStartTime): Promise<
     }, treatmentStartTime - currentTime);
 
   } else if (currentTime < treatmentEndTime) {
+    // If the current time is less than the treatment end time (and greater than the treatment start time because)
+    // of the preceding if-statement, we register the modification Google SERP script.
     registerGoogleModificationScript(conditionType, treatmentEndTime);
   } else {
+    // Otherwise (if the current time is past the treatment end time), we register the default Google SERP script.
     registerGoogleDefaultScript();
   }
 }
 
 async function registerGoogleDefaultScript() {
+  // Unregister any registered Google script.
   if (registeredGoogleScript) {
     registeredGoogleScript.unregister();
     registeredGoogleScript = null;
   }
 
+  // Register the default Google SERP script.
   googleDefaultScript.args["runAt"] = "document_start";
   registeredGoogleScript = await browser.contentScripts.register(googleDefaultScript.args);
 }
 
 async function registerGoogleModificationScript(conditionType, treatmentEndTime) {
+  // Unregister any registered Google script.
   if (registeredGoogleScript) {
     registeredGoogleScript.unregister();
     registeredGoogleScript = null;
   }
 
+  // Register the modification Google SERP script for the participant based on their assigned
+  // condition.
   if (conditionType === "SelfPreferencedRemoval") {
     googleRemoveScript.args["runAt"] = "document_start";
     registeredGoogleScript = await browser.contentScripts.register(googleRemoveScript.args);
@@ -85,6 +98,7 @@ async function registerGoogleModificationScript(conditionType, treatmentEndTime)
   }
 
 
+  // Set a timer to register the default Google serp script upon treatment end time.
   const delay = treatmentEndTime - webScience.timing.now();
   setExtendedTimeout(registerGoogleDefaultScript, delay);
 }
